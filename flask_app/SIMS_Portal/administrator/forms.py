ca76c7bdc1a0507671f57063e3e856bbc09e08a3
Jonathan Garro <jonathan.garro@gmail.com>
2022-12-02 23:47:49 +0000

    removed admin form

--- flask_app/SIMS_Portal/administrator/forms.py
+++ flask_app/SIMS_Portal/administrator/forms.py
@@ -6,7 +6,3 @@ from wtforms import StringField, PasswordField, FileField, SubmitField, BooleanF
 from wtforms_sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from SIMS_Portal.models import User, Emergency, NationalSociety, EmergencyType, Portfolio, Skill, Language
-
-class AdminEditUserForm(FlaskForm):
-	is_admin = SelectField('Admin Status', choices=[0,1])
-	submit = SubmitField('Update User Info')
