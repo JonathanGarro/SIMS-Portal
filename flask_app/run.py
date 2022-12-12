8ecdf5f3cad00ba94582f5eda2242bbd2e370c3b
Jonathan Garro <jonathan.garro@gmail.com>
2022-12-12 16:55:34 +0000

    remove flask autoreloader for cron stability

--- flask_app/run.py
+++ flask_app/run.py
@@ -2,5 +2,6 @@ from SIMS_Portal import create_app
 
 app = create_app()
 
+# turn off user_reloader to prevent duplicate cron jobs
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, use_reloader=False)
