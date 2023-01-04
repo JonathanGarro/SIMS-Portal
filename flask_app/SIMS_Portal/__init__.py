a48b28e5b69ee4377b9e43cfa29fafc97e1e5da7
Jonathan Garro <jonathan.garro@gmail.com>
2023-01-04 21:00:23 +0000

    APscheduler config changes

--- flask_app/SIMS_Portal/__init__.py
+++ flask_app/SIMS_Portal/__init__.py
@@ -56,11 +56,12 @@ def create_app(config_class=Config):
 		print("Flask app running with language set to: {}".format(user_lang))
 		return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 	scheduler = APScheduler()
 	scheduler.init_app(app)
 	scheduler.start()
 	
-	@scheduler.task('cron', id='run_surge_alert_refresh', hour='1')
+	@scheduler.task('cron', id='run_surge_alert_refresh', hour='13')
 	def run_surge_alert_refresh():
 		with scheduler.app.app_context():
 			from SIMS_Portal.alerts.utils import refresh_surge_alerts
