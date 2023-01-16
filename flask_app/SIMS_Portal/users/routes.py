40c4fd6216e044da211586f05993f85cd4aabc77
Jonathan Garro <jonathan.garro@gmail.com>
2023-01-16 13:25:13 +0000

    all member SQL update

--- flask_app/SIMS_Portal/users/routes.py
+++ flask_app/SIMS_Portal/users/routes.py
@@ -20,7 +20,7 @@ def members():
 @users.route('/members/all') 
 @login_required
 def members_all(): 
-	members = db.engine.execute("SELECT u.id, u.firstname, u.lastname, u.status, u.email, u.job_title, u.slack_id, u.ns_id, u.image_file, ns.ns_name, GROUP_CONCAT(DISTINCT l.name) as languages, GROUP_CONCAT(DISTINCT s.name) as skills, GROUP_CONCAT(DISTINCT p.name) as profiles, COUNT(DISTINCT a.id) as assignment_count FROM user u JOIN nationalsociety ns ON ns.ns_go_id = u.ns_id  LEFT JOIN user_language ul ON ul.user_id = u.id LEFT JOIN language l ON l.id = ul.language_id LEFT JOIN assignment a ON a.user_id = u.id  LEFT JOIN user_profile up ON up.user_id = u.id LEFT JOIN profile p ON p.id = up.profile_id LEFT JOIN user_skill us ON us.user_id = u.id LEFT JOIN skill s ON s.id = us.skill_id WHERE u.status = 'Active' GROUP BY u.id ORDER BY u.firstname")
+	members = db.engine.execute("SELECT u.id, u.firstname, u.lastname, u.status, u.email, u.job_title, u.slack_id, u.ns_id, u.image_file, ns.ns_name, GROUP_CONCAT(DISTINCT l.name) as languages, GROUP_CONCAT(DISTINCT s.name) as skills, GROUP_CONCAT(DISTINCT p.name) as profiles, COUNT(DISTINCT a.id) as assignment_count FROM user u JOIN nationalsociety ns ON ns.ns_go_id = u.ns_id  LEFT JOIN user_language ul ON ul.user_id = u.id LEFT JOIN language l ON l.id = ul.language_id LEFT JOIN assignment a ON a.user_id = u.id  LEFT JOIN user_profile up ON up.user_id = u.id LEFT JOIN profile p ON p.id = up.profile_id LEFT JOIN user_skill us ON us.user_id = u.id LEFT JOIN skill s ON s.id = us.skill_id WHERE u.status = 'Active' and assignment_status = 'Active' GROUP BY u.id ORDER BY u.firstname")
 	return render_template('members_all.html', members=members)
 
 @users.route('/register', methods=['GET', 'POST'])
