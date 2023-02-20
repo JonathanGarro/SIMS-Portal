7b0b0311688d1a9574eed84baa060cf339d648ca
Jonathan Garro <jonathan.garro@gmail.com>
2023-02-20 17:52:56 -0500

    Remove missed .gitignore

--- flask_app/run.py
+++ flask_app/run.py
@@ -1,7 +1,11 @@
 from SIMS_Portal import create_app
+import os
 
 app = create_app()
 
 # set use_reloader to false when debugging to avoid duplicate cron job runs
 if __name__ == '__main__':
-	app.run(debug = True, use_reloader = False)
+	app.run(debug = False, use_reloader = False)
+	
+# port = int(os.environ.get("PORT", 5000))
+# app.run(host='0.0.0.0', port=port)
