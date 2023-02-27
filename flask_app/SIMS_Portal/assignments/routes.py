8ff1aa715fadb62e940b7136169ac6f552cd9bda
Jonathan Garro <jonathan.garro@gmail.com>
2023-02-27 19:00:02 +0000

    code cleanup

--- flask_app/SIMS_Portal/assignments/routes.py
+++ flask_app/SIMS_Portal/assignments/routes.py
@@ -4,6 +4,7 @@ from SIMS_Portal.users.utils import send_slack_dm
 from SIMS_Portal import db, login_manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_user, current_user, logout_user, login_required
+from sqlalchemy import func, text
 from SIMS_Portal.assignments.forms import NewAssignmentForm, UpdateAssignmentForm
 from datetime import datetime
 from datetime import date, timedelta
@@ -15,6 +16,18 @@ assignments = Blueprint('assignments', __name__)
 @assignments.route('/assignment/new', methods=['GET', 'POST'])
 @login_required
 def new_assignment():
+	"""
+	Loads the new assignment creation page.
+	
+	Args:
+		None
+	
+	Returns:
+		Renders the new assignment page.
+	
+	Raises:
+		None.
+	"""
 	form = NewAssignmentForm()
 	if form.validate_on_submit():
 		assignment = Assignment(user_id=form.user_id.data.id, emergency_id=form.emergency_id.data.id, start_date=form.start_date.data, end_date=form.end_date.data, role=form.role.data, assignment_details=form.assignment_details.data, remote=form.remote.data)
@@ -27,6 +40,18 @@ def new_assignment():
 @assignments.route('/assignment/new/<int:dis_id>', methods=['GET', 'POST'])
 @login_required
 def new_assignment_from_disaster(dis_id):
+	"""
+	Loads the new assignment creation page for the emergency passed into the function, then saves the new assignment information to the database.
+	
+	Args:
+		dis_id (int) : The unique identifier for the emergency.
+	
+	Returns:
+		Renders the new assignment page.
+	
+	Raises:
+		None
+	"""
 	form = NewAssignmentForm()
 	emergency_info = db.session.query(Emergency).filter(Emergency.id == dis_id).first()
 	if form.validate_on_submit():
@@ -51,6 +76,18 @@ def new_assignment_from_disaster(dis_id):
 @assignments.route('/assignment/<int:id>')
 @login_required
 def view_assignment(id):
+	"""
+	Calculates several figures for visualization of assignment details.
+	
+	Args:
+		id (int) : Assignment ID of the user to view.
+	
+	Returns:
+		Variables to be displayed on the assignment page, including portfolio associated with this assignment.
+	
+	Raises:
+		None.
+	"""
 	assignment_info = db.session.query(Assignment, User, Emergency).join(User).join(Emergency).filter(Assignment.id == id).first()
 	dict_assignment = assignment_info.Assignment.__dict__
 	dict_start_date = str(dict_assignment['start_date'])
@@ -59,13 +96,14 @@ def view_assignment(id):
 	formatted_start_date = datetime.strptime(dict_start_date, '%Y-%m-%d').strftime('%d %b %Y')
 	formatted_end_date = datetime.strptime(dict_end_date, '%Y-%m-%d').strftime('%d %b %Y')
 	
-	days_left = db.engine.execute("SELECT JULIANDAY(:end_date) - JULIANDAY(DATE('now')) AS days_remaining FROM Assignment WHERE id = :id", {'id': id, 'end_date': dict_end_date})
-	days_left_dict = days_left.mappings().first()
-	days_left_int = int(days_left_dict['days_remaining'])
-
-	assingment_length = db.engine.execute("SELECT JULIANDAY(:end_date) - JULIANDAY(:start_date) AS length FROM Assignment WHERE id = :id", {'id': id, 'end_date': dict_end_date, 'start_date': dict_start_date})
-	assignment_length_dict = assingment_length.mappings().first()
-	assignment_length_int = int(assignment_length_dict['length'])
+	start = pd.to_datetime(dict_start_date)
+	end = pd.to_datetime(dict_end_date)
+	diff = end - start
+	assignment_length_int = diff.days
+	
+	today = datetime.today()
+	days_left_int = end - today
+	days_left_int = days_left_int.days
 	
 	assignment_portfolio = db.session.query(Portfolio).filter(Portfolio.assignment_id==id, Portfolio.product_status != 'Removed').all()
 	count_assignment_portfolio = len(assignment_portfolio)
@@ -83,6 +121,18 @@ def view_assignment(id):
 @assignments.route('/assignment/edit/<int:id>', methods=['GET', 'POST'])
 @login_required
 def edit_assignment(id):
+	"""
+	Allows users to edit information with the assignment associated with the passed ID.
+	
+	Args:
+		id (int) : The assignment ID of the record to be edited.
+	
+	Returns:
+		Renders the assignment edit page.
+	
+	Raises:
+		None
+	"""
 	assignment_info = db.session.query(Assignment, User, Emergency).join(User).join(Emergency).filter(Assignment.id == id).first()
 	form = UpdateAssignmentForm(role=assignment_info.Assignment.role, start_date=assignment_info.Assignment.start_date, end_date=assignment_info.Assignment.end_date, assignment_details=assignment_info.Assignment.assignment_details)
 	# get basic info about this assignment
@@ -101,7 +151,20 @@ def edit_assignment(id):
 @assignments.route('/assignment/delete/<int:id>')
 @login_required
 def delete_assignment(id):
-	if current_user.is_admin == 1 or current_user.id == id:
+	"""
+	Deletes the assignment associated with the ID passed. Only allows the person associated with the assignment or a portal administrator to perform.
+	
+	Args:
+		id (int) : The assignment ID of the record to be deleted. Note that this does not delete the record from the databse, but instead toggles the status to "Removed". Administrators should periodically flush those manually to ensure data on associated tables isn't corrupted.
+	
+	Returns:
+		Renders the assignment deletion page, and processes the database changes.
+	
+	Raises:
+		None
+	"""
+	user_assignment = db.session.query(Assignment, User).join(User, User.id == Assignment.user_id).filter(Assignment.id == id).first()
+	if current_user.is_admin == 1 or current_user.id == user_assignment.User.id:
 		try:
 			db.session.query(Assignment).filter(Assignment.id==id).update({'assignment_status':'Removed'})
 			db.session.commit()
@@ -116,8 +179,21 @@ def delete_assignment(id):
 @assignments.route('/assignment/availability/<int:assignment_id>/<start>/<end>', methods=['GET', 'POST'])
 @login_required
 def assignment_availability(assignment_id, start, end):
-	assignment_info = db.session.query(Assignment, User, Emergency).join(User).join(Emergency).filter(Assignment.id == assignment_id).first()
+	"""
+	Generates HTML checkboxes for each date between two dates for the user with the associated assignment ID.
 	
+	Args:
+		assignment_id (int) : Assignment ID of the user.
+		start (str) : Start date of the assignment in yyyy-MM-dd format
+		end (str) : End date of the assignment in yyyy-MM-dd format
+	
+	Returns:
+		Variables to display the availability page, as well as a list of dates generated between the start and end date.
+	
+	Raises:
+		None.
+	"""
+	assignment_info = db.session.query(Assignment, User, Emergency).join(User).join(Emergency).filter(Assignment.id == assignment_id).first()
 	dict_assignment = assignment_info.Assignment.__dict__
 	dict_start_date = str(dict_assignment['start_date'])
 	dict_end_date = str(dict_assignment['end_date'])
@@ -125,24 +201,23 @@ def assignment_availability(assignment_id, start, end):
 	formatted_start_date = datetime.strptime(dict_start_date, '%Y-%m-%d').strftime('%d %b %Y')
 	formatted_end_date = datetime.strptime(dict_end_date, '%Y-%m-%d').strftime('%d %b %Y')
 	
-	days_left = db.engine.execute("SELECT JULIANDAY(:end_date) - JULIANDAY(DATE('now')) AS days_remaining FROM Assignment WHERE id = :id", {'id': assignment_id, 'end_date': dict_end_date})
-	days_left_dict = days_left.mappings().first()
-	days_left_int = int(days_left_dict['days_remaining'])
+	start = pd.to_datetime(dict_start_date)
+	end = pd.to_datetime(dict_end_date)
+	diff = end - start
+	assignment_length_int = diff.days
 	
-	assingment_length = db.engine.execute("SELECT JULIANDAY(:end_date) - JULIANDAY(:start_date) AS length FROM Assignment WHERE id = :id", {'id': assignment_id, 'end_date': dict_end_date, 'start_date': dict_start_date})
-	assignment_length_dict = assingment_length.mappings().first()
-	assignment_length_int = int(assignment_length_dict['length'])
+	today = datetime.today()
+	days_left_int = end - today
+	days_left_int = days_left_int.days
 	
-	start_date = datetime.strptime(start, "%Y-%m-%d")
-	end_date = datetime.strptime(end, "%Y-%m-%d")
-	def daterange(start_date, end_date):
-		for n in range(int((end_date - start_date).days)):
-			yield start_date + timedelta(n)
+	def daterange(start, end):
+		for n in range(int((end - start).days)):
+			yield start + timedelta(n)
 
-	start_day_of_week = pd.Timestamp(start_date)
+	start_day_of_week = pd.Timestamp(start)
 	
 	date_list = []
-	for single_date in daterange(start_date, end_date):
+	for single_date in daterange(start, end):
 		date_list.append(single_date.strftime("%Y-%m-%d"))
 		
 	index_list = []
@@ -156,6 +231,18 @@ def assignment_availability(assignment_id, start, end):
 @assignments.route('/assignment/availability/result', methods=['GET', 'POST'])
 @login_required
 def assignment_availability_result():
+	"""
+	Redirects the user back to their assignment page with their updated availability. Also sends a message confirming the update via Slack.
+	
+	Args:
+		None
+	
+	Returns:
+		Renders the results page.
+	
+	Raises:
+		None
+	"""
 	response = request.form.getlist('available')
 	response_formatted = "{}".format(response)
 	assignment_id = request.form.get('assignment_id')
