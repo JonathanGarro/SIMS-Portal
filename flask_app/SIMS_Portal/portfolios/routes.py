4187de754e6356526527688e8d98bb7a985e91f1
Jonathan Garro <jonathan.garro@gmail.com>
2023-02-28 17:15:41 +0000

    int to bool on portfolio filter

--- flask_app/SIMS_Portal/portfolios/routes.py
+++ flask_app/SIMS_Portal/portfolios/routes.py
@@ -17,14 +17,14 @@ def portfolio():
 	type_search = ''
 	# type_list = ['Map', 'Infographic', 'Dashboard', 'Mobile Data Collection', 'Assessment', 'Internal Analysis', 'External Report', 'Other']
 	type_list = current_app.config['PORTFOLIO_TYPES']
-	public_portfolio = db.session.query(Portfolio).filter(Portfolio.external==1, Portfolio.product_status=='Approved').all()
+	public_portfolio = db.session.query(Portfolio).filter(Portfolio.external==True, Portfolio.product_status=='Approved').all()
 	return render_template('portfolio_public.html', title="SIMS Products", public_portfolio=public_portfolio, type_list=type_list, type_search=type_search)
 	
 @portfolios.route('/portfolio/filter/<type>', methods=['GET', 'POST'])
 def filter_portfolio(type):
 	type_list = current_app.config['PORTFOLIO_TYPES']
 	type_search = "{}".format(type)
-	public_portfolio = db.session.query(Portfolio).filter(Portfolio.external==1, Portfolio.product_status=='Approved', Portfolio.type == type_search).all()
+	public_portfolio = db.session.query(Portfolio).filter(Portfolio.external==True, Portfolio.product_status=='Approved', Portfolio.type == type_search).all()
 	return render_template('portfolio_public.html', title="SIMS Products", public_portfolio=public_portfolio, type_search=type_search, type_list=type_list)
 	
 @portfolios.route('/all_products')
