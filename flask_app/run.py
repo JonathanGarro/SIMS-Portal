9011f02f473b4fb1cc39f502161c694e478a9860
Jonathan Garro <jonathan.garro@gmail.com>
2023-02-28 17:13:25 +0000

    remove old code

--- flask_app/run.py
+++ flask_app/run.py
@@ -5,7 +5,4 @@ app = create_app()
 
 # set use_reloader to false when debugging to avoid duplicate cron job runs
 if __name__ == '__main__':
-	app.run(debug = False, use_reloader = False)
-	
-# port = int(os.environ.get("PORT", 5000))
-# app.run(host='0.0.0.0', port=port)
+	app.run(debug = True, use_reloader = True)
