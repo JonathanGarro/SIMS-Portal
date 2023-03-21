581c0186f89b5b0c11c86906e9cabb96b308e2a3
Jonathan Garro <jonathan.garro@gmail.com>
2023-03-21 13:45:33 +0000

    fix avatar upload path

--- flask_app/SIMS_Portal/users/utils.py
+++ flask_app/SIMS_Portal/users/utils.py
@@ -15,7 +15,7 @@ def save_picture(form_picture):
 	random_hex = secrets.token_hex(8)
 	filename, file_ext = os.path.splitext(form_picture.filename)
 	picture_filename = random_hex + file_ext
-	picture_path = os.path.join(current_app.root_path, 'static/assets/img/avatars', picture_filename)
+	picture_path = os.path.join(current_app.root_path, f'{current_app.config["UPLOAD_FOLDER"]}/assets/img/avatars', picture_filename)
 	
 	output_size = (400, 400)
 	resized_image = Image.open(form_picture)
@@ -29,16 +29,6 @@ def send_reset_slack(user):
 	reset_link = url_for("users.reset_token", token=token, _external=True)
 	msg = "Looks like you requested a password reset. *If you did not request this, simply ignore this message.* Otherwise, follow the directions on the page <{}|linked here>.".format(reset_link)
 	send_slack_dm(msg, user.slack_id)
-
-def send_reset_email(user):
-	token = user.get_reset_token()
-	msg = Message('Password reset request for SIMS', sender='sims_portal@dissolvingdata.com', recipients=[user.email])
-	msg.body = f'''Login issues, huh? No sweat, it happens to the best of us. To reset your password, visit the following link:
-	{url_for("users.reset_token", token=token, _external=True)}
-	
-	If you did not make this request, then simply ignore this email and no changes will be made.
-	'''
-	mail.send(msg)
 
 # send slack alert when new user signs up
 def new_user_slack_alert(message):
