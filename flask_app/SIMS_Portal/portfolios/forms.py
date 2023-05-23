309b38d7f4b3fb2a17a2af6170b296c388fa7835
Jonathan Garro <jonathan.garro@gmail.com>
2023-05-23 09:46:43 -0400

    add code snippet option to portfolio

--- flask_app/SIMS_Portal/portfolios/forms.py
+++ flask_app/SIMS_Portal/portfolios/forms.py
@@ -11,7 +11,7 @@ class PortfolioUploadForm(FlaskForm):
 	emergency_id = QuerySelectField('Emergency', query_factory=lambda:Emergency.query.all(), get_label='emergency_name', allow_blank=True)
 	creator_id = QuerySelectField('Creator', query_factory=lambda:User.query.filter_by(status='Active'), get_label='fullname', allow_blank=True)
 	description = TextAreaField('Description', validators=[DataRequired()])
-	type = SelectField('File Type', choices=['', 'Map', 'Infographic', 'Dashboard', 'Mobile Data Collection', 'Assessment', 'Internal Analysis', 'External Report', 'Other'], validators=[DataRequired()])
+	type = SelectField('File Type', choices=['', 'Map', 'Infographic', 'Dashboard', 'Mobile Data Collection', 'Assessment', 'Internal Analysis', 'External Report', 'Code Snippet', 'Other'], validators=[DataRequired()])
 	file = FileField('Final Product and File Assets (50MB Max)')
 	image_file = FileField('Attach Cover Image (Optional - See Sidebar Guidance)')
 	format = SelectField('Final Product Format', choices=['', 'AI', 'APK', 'CSV', 'DOC', 'DOCX', 'EXE', 'GPKG', 'HTML', 'INDD', 'JAVA', 'JPG', 'JPEG', 'JS', 'JSON', 'KML', 'KMZ', 'PBIX', 'PDF', 'PHP', 'PNG', 'PPT', 'PPTX', 'PSD', 'PY', 'QGIS', 'SHP', 'SQL', 'SVG', 'TXT', 'XLS', 'XLSX', 'XML', 'ZIP', 'OTHER'], validators=[DataRequired()])
