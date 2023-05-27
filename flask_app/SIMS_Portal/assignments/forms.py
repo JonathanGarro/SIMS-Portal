0f7582eef39f77fab62621f3c155be689016a55c
Jonathan Garro <jonathan.garro@gmail.com>
2023-05-27 11:19:34 -0400

    remove FACT/CAP from role type picklist

--- flask_app/SIMS_Portal/assignments/forms.py
+++ flask_app/SIMS_Portal/assignments/forms.py
@@ -8,7 +8,7 @@ from SIMS_Portal.models import User, Emergency, Assignment
 class NewAssignmentForm(FlaskForm):
 	user_id = QuerySelectField('SIMS Member', query_factory=lambda:User.query.filter_by(status='Active').order_by('firstname'), get_label='fullname', allow_blank=True)
 	emergency_id = QuerySelectField('Emergency', query_factory=lambda:Emergency.query.all(), get_label='emergency_name', allow_blank=True)
-	role = SelectField("Role Type", choices=['', 'SIMS Remote Coordinator', 'FACT/CAP Information Management', 'Information Management Coordinator', 'Information Analyst', 'Primary Data Collection Officer', 'Mapping and Visualization Officer', 'Remote IM Support'])
+	role = SelectField("Role Type", choices=['', 'SIMS Remote Coordinator', 'Information Management Coordinator', 'Information Analyst', 'Primary Data Collection Officer', 'Mapping and Visualization Officer', 'Remote IM Support'])
 	start_date = DateTimeField('Start Date', format='%Y-%m-%d')
 	end_date = DateTimeField('End Date', format='%Y-%m-%d')
 	remote = BooleanField('Remote?')
