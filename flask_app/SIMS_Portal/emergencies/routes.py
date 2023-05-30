e6a107f0c31fc75630e9d53f77513ddade46b788
Jonathan Garro <jonathan.garro@gmail.com>
2023-05-30 16:41:53 -0400

    fix for double counting member support

--- flask_app/SIMS_Portal/emergencies/routes.py
+++ flask_app/SIMS_Portal/emergencies/routes.py
@@ -68,10 +68,15 @@ def view_emergency(id):
 		user_is_sims_co = False
 	
 	pending_products = db.session.query(Portfolio).filter(Portfolio.emergency_id == id, Portfolio.product_status == 'Pending Approval').all()
+	
 	deployments = db.session.query(Assignment, Emergency, User, NationalSociety).join(Emergency, Emergency.id==Assignment.emergency_id).join(User, User.id==Assignment.user_id).join(NationalSociety, NationalSociety.ns_go_id==User.ns_id).filter(Emergency.id==id, Assignment.assignment_status=='Active').order_by(User.firstname).all()
+	
 	emergency_info = db.session.query(Emergency, EmergencyType, NationalSociety).join(EmergencyType, EmergencyType.emergency_type_go_id == Emergency.emergency_type_id).join(NationalSociety, NationalSociety.ns_go_id == Emergency.emergency_location_id).filter(Emergency.id == id).first()
+	
 	emergency_portfolio_size = len(db.session.query(Portfolio, Emergency).join(Emergency, Emergency.id == Portfolio.emergency_id).filter(Emergency.id == id, Portfolio.product_status == 'Approved').all())
+	
 	emergency_portfolio = db.session.query(Portfolio, Emergency).join(Emergency, Emergency.id == Portfolio.emergency_id).filter(Emergency.id == id, Portfolio.product_status == 'Approved').limit(3).all()
+	
 	check_for_story = db.session.query(Story, Emergency).join(Emergency, Emergency.id == Story.emergency_id).filter(Story.emergency_id == id).first()
 
 	learning_count = db.session.query(Learning, Assignment, Emergency).join(Assignment, Assignment.id == Learning.assignment_id).join(Emergency, Emergency.id == Assignment.emergency_id).filter(Emergency.id == id).count()
@@ -96,7 +101,7 @@ def view_emergency(id):
 
 	existing_reviews = db.session.query(Review).filter(Review.emergency_id == id).all()
 	
-	deployment_history_count = len(deployments)
+	deployment_history_count = db.session.query(func.count(func.distinct(Assignment.user_id))).filter(Assignment.emergency_id == id).filter(Assignment.assignment_status == 'Active').scalar()
 	
 	try:
 		to_do_trello = get_trello_tasks(emergency_info.Emergency.trello_url)
