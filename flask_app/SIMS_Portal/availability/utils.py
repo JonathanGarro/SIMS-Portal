4551f18a3b5329a15a65c5e8b199cc6635d43938
Jonathan Garro <jonathan.garro@gmail.com>
2023-06-24 10:57:20 -0400

    availability routing

--- flask_app/SIMS_Portal/availability/utils.py
+++ flask_app/SIMS_Portal/availability/utils.py
@@ -1,5 +1,18 @@
+from flask import current_app
 from SIMS_Portal import db
 from datetime import datetime, timedelta
+from slack_sdk import WebClient
+
+def send_slack_availability_request(disaster_id, slack_channel):
+    client = WebClient(token = current_app.config['SIMS_PORTAL_SLACK_BOT'])
+    link = current_app.config['ROOT_URL'] + '/availability/report/' + str(disaster_id)
+    try:
+        result = client.chat_postMessage(
+            channel = slack_channel,
+            text = 'Hello, <!channel>! In order to help the SIMS Remote Coordinator ensure sufficient coverage for this operation, it is requested that you submit your availability for support. The reporting process involves simply checking off the days when you are volunteering to be ready to work on tasks that match your skill set. <{}|Click this link to report.>'.format(link)
+        )
+    except Exception as e:
+        current_app.logger.error('send_slack_availability_request failed: {}'.format(e))
 
 def get_dates_current_and_next_week():
     today = datetime.now().date()
@@ -39,12 +52,19 @@ def get_dates_current_week():
     return readable_dates
 
 def get_dates_next_week():
-    today = datetime.date.today()
-    next_week = today + datetime.timedelta(days=7)
-    dates_next_week = []
+    # Get today's date
+    today = datetime.today()
     
-    for i in range(7):
-        date = next_week + datetime.timedelta(days=i)
-        dates_next_week.append(date)
+    # Find the next Monday
+    current_day = today.weekday()
+    days_ahead = 0 if current_day == 6 else (7 - current_day)
+    next_monday = today + timedelta(days=days_ahead)
     
-    return dates_next_week
+    # Generate the list of next week's days starting from Monday
+    next_week = [next_monday + timedelta(days=i) for i in range(7)]
+    
+    readable_dates = []
+    for date in next_week:
+        readable_dates.append(f"{date.strftime('%A')}, {date.strftime('%B')} {date.day}")
+    
+    return readable_dates
