e0674789d9702180b00ae7322e2155968d47f3ac
Jonathan Garro <jonathan.garro@gmail.com>
2023-08-06 10:20:54 -0400

    set dropdown sort order

--- flask_app/SIMS_Portal/emergencies/forms.py
+++ flask_app/SIMS_Portal/emergencies/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, DateField, DateTimeField, TextAreaField, SelectField, SelectMultipleField, FileField
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import asc
 from flask_wtf.file import FileField, FileAllowed
 from wtforms_sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -9,7 +10,7 @@ from SIMS_Portal.models import User, Emergency, NationalSociety, EmergencyType
 class NewEmergencyForm(FlaskForm):
 	emergency_name = StringField('Emergency Name', validators=[DataRequired(), Length(min=5, max=100)])
 	emergency_location_id = QuerySelectField('Affected Country (Primary)', query_factory=lambda:NationalSociety.query.all(), get_label='country_name', allow_blank=True, validators=[DataRequired()])
-	emergency_type_id = QuerySelectField('Emergency Type', query_factory=lambda:EmergencyType.query.all(), get_label='emergency_type_name', allow_blank=True, validators=[DataRequired()])
+	emergency_type_id = QuerySelectField('Emergency Type', query_factory=lambda:EmergencyType.query.order_by(asc(EmergencyType.emergency_type_name)).all(), get_label='emergency_type_name', allow_blank=True, validators=[DataRequired()])
 	emergency_glide = StringField('GLIDE Number')
 	emergency_go_id = IntegerField('GO ID Number')
 	activation_details = TextAreaField('SIMS Activation Details', validators=[DataRequired()])
