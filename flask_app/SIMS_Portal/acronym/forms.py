f5b8935b59ae927a9c6741c15c3907c6ccc9cfe7
Jonathan Garro <jonathan.garro@gmail.com>
2024-02-20 10:06:00 -0500

    add new acronym form

---
+++ flask_app/SIMS_Portal/acronym/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, DateField, DateTimeField, TextAreaField, SelectField, SelectMultipleField
+from flask_sqlalchemy import SQLAlchemy
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from SIMS_Portal.models import User, Acronym
+
+class NewAcronymForm(FlaskForm):
+    acronym_eng = StringField('Acronym (English)')
+    def_eng = TextAreaField('Definition (English)', render_kw={'style':'height: 100px'})
+    expl_eng = TextAreaField('Explanation (English)', render_kw={'style':'height: 100px'})
+    acronym_esp = StringField('Acronym (Spanish)')
+    def_esp = TextAreaField('Definition (Spanish)', render_kw={'style':'height: 100px'})
+    expl_esp = TextAreaField('Explanation (Spanish)', render_kw={'style':'height: 100px'})
+    acronym_fra = StringField('Acronym (French)')
+    def_fra = TextAreaField('Definition (French)', render_kw={'style':'height: 100px'})
+    expl_fra = TextAreaField('Explanation (French)', render_kw={'style':'height: 100px'})
+    relevant_link = StringField('Relevant URL (if Applicable)')
+    
+    submit = SubmitField('Submit Acronym')
