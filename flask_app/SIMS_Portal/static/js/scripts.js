970fdd7c365757fb875f45c62775f1d54546c98d
Jonathan Garro <jonathan.garro@gmail.com>
2024-06-14 11:54:40 -0400

    remove old js

--- flask_app/SIMS_Portal/static/js/scripts.js
+++ flask_app/SIMS_Portal/static/js/scripts.js
@@ -351,42 +351,6 @@ $(document).ready(function () {
 	});
 });
 
-$(function(){
-	$(".typed").typed({
-		strings: ["infographics.", "mobile data collection.", "basemaps."],
-		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-		stringsElement: null,
-		// typing speed
-		typeSpeed: 30,
-		// time before typing starts
-		startDelay: 1200,
-		// backspacing speed
-		backSpeed: 20,
-		// time before backspacing
-		backDelay: 500,
-		// loop
-		loop: true,
-		// false = infinite
-		loopCount: 5,
-		// show cursor
-		showCursor: false,
-		// character for cursor
-		cursorChar: "|",
-		// attribute to type (null == text)
-		attr: null,
-		// either html or text
-		contentType: 'html',
-		// call when done callback function
-		callback: function() {},
-		// starting callback function before each string
-		preStringTyped: function() {},
-		//callback for every typed string
-		onStringTyped: function() {},
-		// callback for reset
-		resetCallback: function() {}
-	});
-});
-
 $(document).ready(function() {
   $("h5").html(function(_, html) {
 	return html.replace(/(\#\w+)/g, '<span class="tweet">$1</span>');
