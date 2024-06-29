b458e3ef9874857019fbf6e9cf1dd0b93319bffd
Jonathan Garro <jonathan.garro@gmail.com>
2024-06-29 12:14:21 -0400

    add manual slack message class

--- flask_app/SIMS_Portal/main/forms.py
+++ flask_app/SIMS_Portal/main/forms.py
@@ -1,12 +1,17 @@
 from flask_wtf import FlaskForm
 from flask_sqlalchemy import SQLAlchemy
-from wtforms import StringField, SubmitField, BooleanField, IntegerField, DateField, DateTimeField, SelectField, SelectMultipleField, HiddenField, FileField
+from wtforms import StringField, SubmitField, BooleanField, IntegerField, DateField, DateTimeField, SelectField, SelectMultipleField, HiddenField, FileField, TextAreaField
 from wtforms.widgets import TextArea
 from wtforms_sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_wtf.file import FileField, FileAllowed
 from SIMS_Portal import db
 from SIMS_Portal.models import User, Emergency, Portfolio, Skill, Language, EmergencyType, NationalSociety, Badge, Assignment
+
+class ManualSlackMessage(FlaskForm):
+	message = TextAreaField('Message', validators=[DataRequired()], render_kw={'style':'height: 100px'})
+	user_slack = QuerySelectField('Member', query_factory=lambda: User.query.order_by(User.firstname).all(), get_label='fullname', allow_blank=True)
+	submit = SubmitField('Send Message')
 
 class MemberSearchForm(FlaskForm):
 	name = StringField('Member Name')
