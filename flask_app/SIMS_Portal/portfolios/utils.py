f102849ce427d02423110fd64b66be5b1d31a48e
Jonathan Garro <jonathan.garro@gmail.com>
2024-07-06 09:25:15 -0400

    add docstrings

--- flask_app/SIMS_Portal/portfolios/utils.py
+++ flask_app/SIMS_Portal/portfolios/utils.py
@@ -12,10 +12,37 @@ import logging
 
 
 def save_portfolio_to_dropbox(form_file, user_id, type):
+	"""
+	Save a portfolio file to Dropbox and return the file details and shared link.
+	
+	Generates a unique filename for the uploaded file, saves it locally, uploads it to Dropbox,
+	and returns the file name and a shared link to the file.
+	
+	Parameters:
+	form_file (FileStorage): The file object to be uploaded.
+	user_id (int): The ID of the user uploading the file.
+	type (str): The type of file being uploaded.
+	
+	Returns:
+	dict: A dictionary containing:
+		- 'file_filename' (str): The name of the file saved in Dropbox.
+		- 'share_link' (str): The shared link to the uploaded file in Dropbox.
+	
+	Side Effects:
+	- Saves the file locally before uploading it to Dropbox.
+	- Uploads the file to a specific folder in Dropbox.
+	- Creates a shared link for the uploaded file in Dropbox.
+	
+	Raises:
+	None
+	"""
+	
 	# generate unique string to avoid filename conflicts
 	random_hex = secrets.token_hex(8)
+	
 	# use an access token with ONLY individual scopes (don't select any team scopes)
 	dropbox_access_token = current_app.config['DROPBOX_BOT']
+	
 	filename, file_ext = os.path.splitext(form_file.filename)
 	file_filename = type + '-user'+ str(user_id) + '-' + random_hex + file_ext
 	local_file_path = os.path.join(current_app.root_path, 'static/assets/portfolio', file_filename)
@@ -42,6 +69,29 @@ def save_portfolio_to_dropbox(form_file, user_id, type):
 	return output
 
 def save_cover_image(form_file, user_id, type):
+	"""
+	Save a cover image to an S3 bucket after resizing it.
+	
+	Generates a unique filename for the uploaded cover image, downscales the image to a maximum width of 850 pixels,
+	saves it to a temporary file, uploads it to an S3 bucket, and returns the file path.
+	
+	Parameters:
+	form_file (FileStorage): The image file to be uploaded.
+	user_id (int): The ID of the user uploading the image.
+	type (str): The type of image being uploaded.
+	
+	Returns:
+	str: The file path where the image is saved in the S3 bucket.
+	
+	Side Effects:
+	- Saves the resized image to a temporary file before uploading it to S3.
+	- Uploads the image file to a specified S3 bucket.
+	- Logs an error if resizing the image fails.
+	
+	Raises:
+	None
+	"""
+	
 	random_hex = secrets.token_hex(8)
 	filename, file_ext = os.path.splitext(form_file.filename)
 	file_filename = type + '-user'+ str(user_id) + '-' + random_hex + file_ext
@@ -64,7 +114,24 @@ def save_cover_image(form_file, user_id, type):
 	return file_path
 
 def get_full_portfolio(id):
-	"""Takes in a user's ID and gets all of their products, including those that they are listed as the creator (original poster to the portal) and those that they tagged themselves as a collaborator"""
+	"""
+	Retrieve the full portfolio of a user by their ID.
+	
+	Queries the database to get all portfolio items associated with a specific user, either as a creator
+	or as a collaborator. It returns a list of portfolio items that are not marked as 'Removed'.
+	
+	Parameters:
+	id (int): The ID of the user for whom to retrieve the portfolio.
+	
+	Returns:
+	list: A list of Portfolio objects associated with the user.
+	
+	Side Effects:
+	None
+	
+	Raises:
+	None
+	"""
 	
 	user_info = db.session.query(User).filter(User.id == id).first()
 	all_collaborators_plus_creators = db.session.query(Portfolio.id, Portfolio.collaborator_ids, Portfolio.creator_id, Portfolio.product_status).filter(Portfolio.product_status != 'Removed').all()
