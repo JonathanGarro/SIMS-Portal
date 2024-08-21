ff20885cd6ff7bfd6eba98a752e9d57271479949
Jonathan Garro <jonathangarro@jonathans-mbp-m3-max.local>
2024-08-21 10:17:05 -0400

    add docstrings

--- flask_app/SIMS_Portal/acronym/routes.py
+++ flask_app/SIMS_Portal/acronym/routes.py
@@ -43,6 +43,25 @@ def acronyms():
 
 @acronym.route('/acronyms/search', methods=['GET'])
 def search_acronyms():
+    """
+    Handle the GET request to retrieve and display a paginated list of approved acronyms.
+    
+    This endpoint retrieves a paginated list of acronyms from the database that have been approved
+    (i.e., have a non-zero 'approved_by' field). The acronyms are sorted alphabetically by their
+    English representation.
+    
+    The pagination is determined by the 'page' query parameter, with a default of 100 acronyms per page.
+    
+    The function also checks if the current user is an admin to determine if they have editing privileges.
+    Additionally, it attempts to retrieve the current user's information from the database.
+    
+    Returns:
+        Response: Renders the 'acronyms.html' template with the following context:
+            - paginated_acronyms: A paginated list of approved acronyms.
+            - user_is_admin: A boolean indicating whether the current user has admin privileges.
+            - user_info: The current user's information, or None if not found.
+    """
+    
     search_column = request.args.get('search_column', 'acronym_eng')
     search_term = request.args.get('search_term', '')
 
@@ -88,6 +107,33 @@ def search_acronyms():
 
 @acronym.route('/acronyms/api/search', methods=['GET'])
 def search_acronyms_api():
+    """
+    Handle the GET request to search for acronyms based on a specified column and search term.
+    
+    Endpoint allows users to search for acronyms in the database based on a specified column 
+    and search term. The search is case-insensitive and is restricted to acronyms that have been 
+    approved (i.e., have a non-zero 'approved_by' field).
+    
+    The column to search on is determined by the 'search_column' query parameter, with a default 
+    of 'acronym_eng'. The search term is provided via the 'search_term' query parameter.
+    
+    The search results are returned in JSON format, including relevant fields for each matching acronym.
+    
+    Query Parameters:
+        search_column (str): The column to search within the Acronym model (default is 'acronym_eng').
+        search_term (str): The term to search for within the specified column.
+    
+    Returns:
+        Response: A JSON object containing the search results, where each result includes:
+            - acronym_eng: The English version of the acronym.
+            - def_eng: The English definition of the acronym.
+            - acronym_esp: The Spanish version of the acronym.
+            - def_esp: The Spanish definition of the acronym.
+            - acronym_fra: The French version of the acronym.
+            - def_fra: The French definition of the acronym.
+            - link: A relevant link associated with the acronym.
+    """
+    
     search_column = request.args.get('search_column', 'acronym_eng')
     search_term = request.args.get('search_term', '')
 
@@ -116,6 +162,28 @@ def search_acronyms_api():
 
 @acronym.route('/view_acronym/<int:id>')
 def view_acronym(id):
+    """
+    Handle the GET request to view details of a specific acronym by its ID.
+    
+    Endpoint retrieves detailed information about a specific acronym, including the user who 
+    added it, based on the acronym's unique ID. The information is gathered by joining the `Acronym` 
+    and `User` models. If the acronym is not found, a 404 error is returned.
+    
+    Additionally, the function attempts to find other acronyms with the same English representation 
+    (`acronym_eng`) but with different IDs, which are considered similar matches.
+    
+    Parameters:
+        id (int): The unique identifier of the acronym to be viewed.
+    
+    Returns:
+        Response: If the acronym is found, it returns the details of the acronym and any similar matches.
+                  If the acronym is not found, a 404 error is returned.
+    
+    Exceptions:
+        - 404: If no acronym with the specified ID is found.
+        - 404: If no results are found when querying the database.
+    """
+    
     try:
         acronym_info = db.session.query(Acronym, User).join(User, User.id == Acronym.added_by).filter(Acronym.id == id).first()
         
@@ -134,9 +202,17 @@ def view_acronym(id):
 @acronym.route('/submit_acronym')
 def submit_acronym():
     """
-    There are two ways to submit acroynms: as a logged in member and as an anonymous visitor. 
-    This function checks to see which this user is and routes them to the appropriate submission function.
+    Handle the submission of acronyms by routing users to the appropriate submission function based on their authentication status.
+    
+    There are two ways to submit acronyms:
+    1. As a logged-in member: If the user is authenticated, they are redirected to the member-specific acronym submission function.
+    2. As an anonymous visitor: If the user is not authenticated, they are redirected to the public (anonymous) acronym submission function.
+    
+    Returns:
+        Response: A redirect to either the 'submit_acronym_member' function for authenticated users or 
+                  the 'submit_acronym_public' function for anonymous visitors.
     """
+    
     if not current_user.is_authenticated:
         return redirect(url_for('acronym.submit_acronym_public'))
     else:
@@ -145,6 +221,36 @@ def submit_acronym():
 @acronym.route('/submit_acronym/member', methods=['GET', 'POST'])
 @login_required
 def submit_acronym_member():
+    """
+    Handle the submission of a new acronym by a logged-in member.
+    
+    Function allows authenticated users to submit a new acronym. It supports both GET and POST requests:
+    
+    - GET request: 
+        Renders the form for submitting a new acronym. Also retrieves and displays the 10 most recent acronyms 
+        submitted by users.
+    
+    - POST request:
+        Processes the acronym submission form. If the form is valid, a new acronym is created in the database 
+        with the current user's ID as the submitter. The new acronym is automatically approved with a hard-coded 
+        'approved_by' value of 63 (Clara Barton). After the acronym is added to the database, a log entry is 
+        created, and an alert is generated to notify relevant users.
+    
+    Parameters:
+        None (the function relies on form data and the current user's session).
+    
+    Returns:
+        Response:
+            - For GET requests: Renders the 'new_acronym.html' template with the form and latest acronyms.
+            - For POST requests:
+                - On success: Redirects to the acronym submission page with a success message.
+                - On failure: Redirects to the acronym submission page with error messages.
+    
+    Exceptions:
+        - The function handles form validation errors by flashing error messages.
+        - Attempts to send an acronym alert email, but passes silently on failure.
+    """
+    
     form = NewAcronymForm()
     
     if request.method == 'GET': 
@@ -199,6 +305,40 @@ def submit_acronym_member():
 
 @acronym.route('/submit_acronym/public', methods=['GET', 'POST'])
 def submit_acronym_public():
+    """
+    Handle the submission of a new acronym by an anonymous user.
+    
+    Function allows non-authenticated (anonymous) users to submit a new acronym. It supports both 
+    GET and POST requests:
+    
+    - GET request:
+        Renders the form for submitting a new acronym. Additionally, retrieves and displays the 
+        10 most recent acronyms submitted by users.
+    
+    - POST request:
+        Processes the acronym submission form. If the form is valid, a new acronym is created in 
+        the database with the submission credited to a placeholder account (user ID 63, representing 
+        Clara Barton). The acronym is not automatically approved and is queued for review. After the 
+        acronym is added, a log entry is created, and an alert is generated to notify administrators 
+        of the new submission.
+    
+    Parameters:
+        None (the function relies on form data submitted by the user).
+    
+    Returns:
+        Response:
+            - For GET requests: Renders the 'new_acronym.html' template with the form and latest acronyms.
+            - For POST requests:
+                - On success: Redirects to the acronym list page with a success message indicating that 
+                              the acronym has been added to the review queue.
+                - On failure: Logs the error, flashes an error message, and redirects back to the form.
+    
+    Exceptions:
+        - The function logs any errors encountered during the submission process and flashes error 
+          messages for form validation failures.
+        - Attempts to send an acronym alert email, but passes silently on failure.
+    """
+    
     form = NewAcronymFormPublic()
 
     if request.method == 'GET': 
@@ -255,10 +395,31 @@ def submit_acronym_public():
 
     return redirect(url_for('acronym.submit_acronym_public'))
 
-
 @acronym.route('/acronym/approve/<int:id>', methods=['GET', 'POST'])
 @login_required
 def approve_acronym(id):
+    """
+    Handle the approval of an acronym by an admin.
+    
+    Function allows an authenticated admin user to approve an acronym by setting the 
+    'approved_by' field to the admin's user ID. Upon approval, the acronym becomes visible 
+    to all users.
+    
+    Parameters:
+        id (int): The unique identifier of the acronym to be approved.
+    
+    Returns:
+        Response:
+            - If the current user is an admin:
+                - The acronym's 'approved_by' field is updated with the admin's ID.
+                - A success message is flashed, and the user is redirected to the acronym 
+                  administration page (`admin_process_acronyms`).
+            - If the current user is not an admin:
+                - Renders a template displaying a list of admin users (`portal_admins.html`), 
+                  indicating that the current user does not have the necessary privileges to 
+                  approve the acronym.
+    """
+    
     if current_user.is_admin == 1:
         db.session.query(Acronym).filter(Acronym.id == id).update({'approved_by':current_user.id})
         db.session.commit()
@@ -271,6 +432,28 @@ def approve_acronym(id):
 @acronym.route('/acronym/delete/<int:id>', methods=['GET', 'POST'])
 @login_required
 def delete_acronym(id):
+    """
+    Handle the deletion of an acronym by an admin.
+    
+    Function allows an authenticated admin user to delete an acronym from the database. 
+    The acronym is identified by its unique ID. Upon successful deletion, a success message 
+    is flashed, and the user is redirected to the acronym administration page.
+    
+    Parameters:
+        id (int): The unique identifier of the acronym to be deleted.
+    
+    Returns:
+        Response:
+            - If the current user is an admin:
+                - The specified acronym is deleted from the database.
+                - A success message is flashed, and the user is redirected to the acronym 
+                  administration page (`admin_process_acronyms`).
+                - If an error occurs during deletion, the operation is rolled back, an error 
+                  message is flashed, and the error is logged. The user is then redirected 
+                  to the acronym administration page.
+            - If the acronym does not exist, the user is redirected to the acronym administration page.
+    """
+    
     if current_user.is_admin == 1:
         acronym_to_delete = db.session.query(Acronym).filter(Acronym.id == id).first()
         if acronym_to_delete:
@@ -294,6 +477,36 @@ def delete_acronym(id):
 @acronym.route('/acronym/edit/<int:id>', methods=['GET', 'POST'])
 @login_required
 def edit_acronym(id):
+    """
+    Handle the editing of an existing acronym.
+    
+    Function allows authenticated users to edit an existing acronym. The user must either be 
+    the original submitter of the acronym or an admin to make changes. The function supports both 
+    GET and POST requests:
+    
+    - GET request:
+        Retrieves the existing acronym data and pre-fills the editing form. If the acronym does 
+        not exist, a 404 error is returned.
+    
+    - POST request:
+        Validates the form data and updates the acronym in the database. If the update is successful, 
+        a log entry is created, and the user is redirected to the view page for the acronym. If the 
+        user is not authorized to edit the acronym, a 403 error is returned.
+    
+    Parameters:
+        id (int): The unique identifier of the acronym to be edited.
+    
+    Returns:
+        Response:
+            - For GET requests: Renders the 'acronym_edit.html' template with the form pre-filled 
+              with the existing acronym data.
+            - For POST requests:
+                - On successful validation and update: Redirects to the view page of the acronym 
+                  with a success message.
+                - On failure to validate or unauthorized access: Aborts with the appropriate HTTP 
+                  status code (404 for not found, 403 for unauthorized).
+    """
+    
     acronym_info = db.session.query(Acronym).filter(Acronym.id == id).first()
     form = EditAcronymForm(obj=acronym_info)  # Initialize form with acronym_info data
 
